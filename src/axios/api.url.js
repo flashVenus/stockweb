861afb745b99cc6ba8a142e52c584445d51e997a
wall <wall@example.com>
2022-04-04 14:23:31 +0800

    'first'

---
+++ src/axios/api.url.js
@@ -0,0 +1,9 @@
+export default {
+  DOMAIN: 'http://sr.nqsoft.net/',
+  // baseURL: 'http://www.shehua56.com',
+  baseURL: 'http://sr.nqsoft.net/',
+
+  util: {
+    image: '/util/image.html' // 图片上传
+  }
+}
